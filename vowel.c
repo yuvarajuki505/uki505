73746c3c5f268b69779f67eae0aada2e4227a482
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-01-18 10:59:39 +0000

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter any charecter");
+scanf("%c",&ch);
+if(ch='a'||ch='e'|ch='i'|ch='o'|ch='u')
+{
+printf("these are vowels");
+}
+else
+{
+printf("these are conconant");
+}
+return0;
+}
