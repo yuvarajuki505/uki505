4b4fd4a1ff42e2eb55d6faefee98af237d2f8359
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-01-18 11:10:47 +0000

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter the charecter");
+scanf("%d",&c);
+if((c>='a'&&c<='z')||(c>='A'&&c<='z'))
+{
+printf("charecter is alphabet");
+}
+else
+{
+printf("charecter is not alphabet")
+}
+return0;
+}
