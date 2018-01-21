9713fa75d71c54b6a08761460c666ca0257cc7b8
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-01-21 13:51:31 +0530

    Create armstrong interval.c

---
+++ armstrong interval.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+int main()
+{
+intn1,n2,i,temp,number,rem;
+printf("Enter two number(intervals):");
+scanf("%d%d",&n1&n2);
+printf("Amstrong number between% an %d are:"n1,n2);
+for(i=n1+;i<n2;++1);
+{
+temp+i;
+number=0;
+while (temp!=0)
+{
+rem=(temp%10);
+num+=rem*rem*rem;
+temp 1=10;
+}
+if(i==num)
+{
+printf("%d",i);
+}
+}
+return0;
+}
