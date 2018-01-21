c144f78b5b9c9b74e57ebd8da29914988ebb4324
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-01-21 14:31:05 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+ int hrs,min;
+ printf("enter the minutes");
+ scanf("%d",&min);
+ hrs=min*1/60;
+ printf("the hours is %d the minutes id %d",hrs,min);
+ return0;
+}
