380138591f06177a7af8c4699b0c085633900a64
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-01-21 13:38:40 +0530

    Create mult.c

---
+++ mult.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main() 
+{ 
+int i, d,res ;   
+printf(" \nEnter the num ") ; 
+scanf("%d",&d) ; 
+printf(" \n The num are ") ; 
+for(i=1;i<=7;i++) 
+{
+res=d*i;
+printf("%d",res) ; 
+}
+return0;
+]
