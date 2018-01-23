b1266eb9bdeba7f27def5fa23711690cbb6b8973
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-01-23 10:17:42 +0000

    Create numcharstrng.c

---
+++ numcharstrng.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+char str[150];
+int i = 0, l = 0, f = 1;
+printf("Enter any string\n");
+scanf("%s",&str);
+if(str==1 || str==9)
+{
+l=l++;
+printf("the no of charcter in string is %d\n",l)
+getch();
+return 0;
+}
