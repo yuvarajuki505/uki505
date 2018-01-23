2405f6e331cfd8564b2a1b0e101bc651311d1a90
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-01-23 11:26:40 +0000

    Create strngktimes.c

---
+++ strngktimes.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(0)
+{
+    char word[20];
+    int k,i;
+    printf("Enter the word");
+    scanf("%s",word);
+    printf("Enter the number of times to print");
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+        printf("%s",word);
+}
+return0;
+}
