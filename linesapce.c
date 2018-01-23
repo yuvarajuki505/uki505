4742a66668188e9ed6e70671f8e4941110f19ffe
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-01-23 10:11:37 +0000

    Create linesapce.c

---
+++ linesapce.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char a[20];
+int b,spc=0;
+printf("enter the string");
+scanf("%s",&a[i]);
+while(a[b]!='\0')
+{
+if(!(a[b]>='a')&&(a[b]<='z')))
+{
+spc=spc+1;
+}
+b++;
+}
+printf("%d",spc);
+return0;
+}
