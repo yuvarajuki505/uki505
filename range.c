57f599af8f8e04b79723fe5f0834290256327028
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-02-01 14:04:33 +0530

    Update range.c

--- range.c
+++ range.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int n;
-printf("enter the number\n");
-scanf("%d",&n);
-if(n>=1&&n<=10)
-{
-printf("yes\n");
-}
-else
-{
-printf("no\n");
-}
-return 0;
+	int n,i;
+	printf("enter the value:\n");
+	scanf("%d",&n);
+	if(n>=1&&n<=10)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
 }
