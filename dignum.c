2dacc7ff67dfa70a04204eb6eea5ee09d533402c
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-02-01 15:11:04 +0530

    Create dignum.c

---
+++ dignum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+printf("enter the number");
+switch(n)
+{
+case1:printf("one");
+break;
+case2:printf("two");
+break;
+case3:printf("three");
+break;
+case4:printf("four");
+break;
+case5:printf("five");
+break;
+default:
+printf("not available");
+break;
+}
+return 0;
+}
+
