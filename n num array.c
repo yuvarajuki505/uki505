853fa921c773e1d8b4a5d3e3fb1c18ea39a0f3c2
yuvarajuki505 <31504755+yuvarajuki505@users.noreply.github.com>
2018-02-01 14:24:41 +0530

    Create n num array.c

---
+++ n num array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int i, a[5],avg,sum=0;
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		sum=sum+a[i];
+	}
+	avg=sum/5;
+	printf("%d",avg);
+	
+	
+	return 0;
+}
